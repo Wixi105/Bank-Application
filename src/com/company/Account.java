2642e23d43290198d8f2b94bfacbc481ab86f481
Wixi105 <ericsampson105@gmail.com>
2020-11-04 23:38:43 +0000

    user data now goes into the database

--- src/com/company/Account.java
+++ src/com/company/Account.java
@@ -2,7 +2,9 @@ package com.company;
 
 public class Account {
 
-    private String name;
+    private String firstName;
+    private String middleName;
+    private String lastName;
     public static int incrementalVal;
     private String number;
 //    private String dateCreated;
@@ -11,8 +13,10 @@ public class Account {
     private int balance = 0;
     
     
- public Account(String name, String number, String email, String phoneNo,int balance){
-        this.name = name;
+ public Account(String firstName, String middleName, String lastName, String number, String email, String phoneNo,int balance){
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
         this.number = number;
         this.email = email;
 //        this.dateCreated = dateCreated;
@@ -20,16 +24,6 @@ public class Account {
         this.balance = balance;
         incrementalVal++;
 
-    }
-
-    public void deposit(int amount){
-        if(amount > 0){
-            balance += amount;
-            System.out.println("You have successfully deposited " + amount + " into your account.");
-            System.out.println("Your current balance is " + balance);
-        }else{ 
-            System.out.println("Amount is not valid");
-        }
     }
 
     public void withdraw(int amount){
@@ -41,8 +35,16 @@ public class Account {
         }
     }
 
-	public String getName() {
-		return name;
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
     }
 
     public static int getIncrementalVal() {
@@ -63,6 +65,10 @@ public class Account {
 
     public int getBalance() {
         return balance;
+    }
+
+    public String getFullName(){
+     return getFirstName() + getMiddleName() + getLastName();
     }
 
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,33 +8,15 @@ import java.util.Scanner;
 
 
 public class Main {
-//    private static final Date date = new Date();
-//    private static final SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
-    private static final Scanner scanner = new Scanner(System.in);
-    private static ArrayList<Account> accounts = new ArrayList<>();
-    private static  String firstName;
-    private static  String middleName;
-    private static  String lastName;
-
+private static final Scanner scanner = new Scanner(System.in);
+    private static final ArrayList<Account> accounts = new ArrayList<>();
 
 
 
     public static void main(String[] args) {
-        try {
-//           Class.forName("org.mariadb.jdbc.Driver");
-            String dbUrl = "jdbc:mysql://localhost/hulubank";
-            String dbUser = "odin";
-            String dbPass = "123456";
-            Connection conn = DriverManager.getConnection(dbUrl,dbUser,dbPass);
-            System.out.println("Connection Successful");
-            Account account1 = createAccount();
-            Statement statement = conn.createStatement();
-            String sql = "INSERT INTO Customers(FIRSTNAME,MIDDLENAME,LASTNAME,EMAIL,ACCOUNT_NUMBER,PHONENO) " + " VALUES('"firstName"',' ','"lastName"','eric@gmail.com','1123456','0203663447');";
-            statement.executeQuery(sql);
 
-        } catch (SQLException e) {
-            throw new Error("Error ", e);
-        }
+        Account account1 = createAccount();
+
 
     }
 
@@ -43,8 +25,14 @@ public class Main {
             System.out.println("Welcome to First Hulu Bank");
     
             //Taking the name of account
-            System.out.print("Enter your full name (first middle last): ");
-            String name = scanner.nextLine();
+            System.out.print("Enter your first name: ");
+            String firstName = scanner.nextLine();
+
+            System.out.print("Enter your middle name: ");
+            String middleName = scanner.nextLine();
+
+            System.out.print("Enter your last name: ");
+            String lastName = scanner.nextLine();
     
             System.out.print("Enter your email address: ");
             String email = scanner.nextLine();
@@ -53,57 +41,88 @@ public class Main {
             String phoneNo = scanner.nextLine();
     
             //splitting and working with text.
-            String[] nameSplit = name.split(" ");
+
             String accountBaseNumber = "HULU-5000";
             String[] splitBase = accountBaseNumber.split("-");
             int nextAccountNumber = Integer.parseInt(splitBase[1]) + Account.incrementalVal;
             String accountNumber = "HULU-"+ nextAccountNumber +"";
             int balance = 0;
 
-        switch (nameSplit.length) {
-            //                if user has three names. e.g John Michael Doe
-            case 3 -> {
-                firstName = nameSplit[0];
-                middleName = nameSplit[1];
-                lastName = nameSplit[2];
-            }
-            //                    if user has four names and in case last two names are surnames e.g. John Michael Doe Raymond.
-            case 4 -> {
-                firstName = nameSplit[0];
-                middleName = nameSplit[1];
-                lastName = nameSplit[2] + " " + nameSplit[3];
-            }
-            default -> System.out.println("Default Printout");
-        }
-
             System.out.println("Hello " + firstName +" "+ lastName + ", your account number is: " + accountNumber + " with a balance of " + balance);
     
             //getting date.
-//            String dateCreated = formatter.format(date);
-//            System.out.println("This account was created on " + dateCreated);
 
-        return new Account(name,accountNumber,email,phoneNo,balance);
-        
+        try {
+//           Class.forName("org.mariadb.jdbc.Driver");
+            String dbUrl = "jdbc:mysql://localhost/hulubank";
+            String dbUser = "odin";
+            String dbPass = "123456";
+            Connection conn = DriverManager.getConnection(dbUrl,dbUser,dbPass);
+            String sql = "INSERT INTO Customers(FIRSTNAME,MIDDLENAME,LASTNAME,EMAIL,ACCOUNT_NUMBER,PHONENO) VALUES(?,?,?,?,?,?);";
+            PreparedStatement pstatement = conn.prepareStatement(sql);
+
+            pstatement.setString(1,firstName);
+            pstatement.setString(2,middleName);
+            pstatement.setString(3,lastName);
+            pstatement.setString(4,email);
+            pstatement.setString(5,accountNumber);
+            pstatement.setString(6,phoneNo);
+            pstatement.executeUpdate();
+
+            boolean check = true;
+            while(check){
+                System.out.println("Personal Information has been stored. Press 1 to initiate deposit or 2 to cancel");
+                int userOption = scanner.nextInt();
+                switch (userOption){
+                    case 1 -> {
+                        int depositAmount = depositAmount();
+                        String sql2 = "UPDATE Customers SET BALANCE = ? WHERE ACCOUNT_NUMBER = ?";
+                        PreparedStatement preparedStatement = conn.prepareStatement(sql2);
+                        preparedStatement.setInt(1,depositAmount);
+                        preparedStatement.setString(2,accountNumber);
+                        preparedStatement.executeUpdate();
+                        System.out.print("Your balance is now: " + depositAmount);
+                        check = false;
+                    }
+                    case 2 ->{
+                        check = false;
+                        System.out.print("Your account balance remains : " + balance);
+                    }
+                }
+
+            }
+
+        } catch (SQLException e) {
+            throw new Error("Error ", e);
+        }
+
+        return new Account(firstName,middleName,lastName,accountNumber,email,phoneNo,balance);
+
+    }
+
+
+    public static int depositAmount(){
+        System.out.print("Enter amount to deposit: ");
+        return scanner.nextInt();
     }
 
     public static void removeAccount(Account account){
         if(accounts.contains(account)){
-            System.out.println("The account with account name "+ account.getName() +" is being removed. Press 1 to Confirm or 2 to Cancel");
+            System.out.println("The account with account name "+ account.getFullName() +" is being removed. Press 1 to Confirm or 2 to Cancel");
             int check = scanner.nextInt();
             if(check == 1){
                 accounts.remove(account);
                 System.out.println("Account removed successfully");
             }else{
                 System.out.println("Account failed to be removed");
-                return;
             }
         }
     }
 
     public static void searchAccount(String accountName){
-        for(int i =0; i < accounts.size(); i++){
-            String nameInAccounts = accounts.get(i).getName();
-            if(nameInAccounts.equals(accountName)){
+        for (Account account : accounts) {
+            String nameInAccounts = account.getFullName();
+            if (nameInAccounts.equals(accountName)) {
                 System.out.println("Account exists");
             }
         }
